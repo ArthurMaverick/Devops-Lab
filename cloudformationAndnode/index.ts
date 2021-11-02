f67cd7f81cd32b96017c5912e6ae437879c54bff
ArthurMaverick <arthuracs18@gmail.com>
2021-11-02 11:14:03 -0300

    add - example - 1

--- cloudformationAndnode/index.ts
+++ cloudformationAndnode/index.ts
@@ -9,7 +9,7 @@ StackName: 'RANDOM'
 }
 
 
-function CreateStack(template: CloudFormation.CreateStackInput) {
+function createStack(template: CloudFormation.CreateStackInput) {
   const cfn = new CloudFormation({apiVersion: '2010-09-09'})
   cfn.createStack(template, callback => console.log(callback))
 }
@@ -18,5 +18,4 @@ function template (params: CloudFormation.CreateStackInput) {
   return params
 }
 
-
-CreateStack(template(values))
+createStack(template(values))
