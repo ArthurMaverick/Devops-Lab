879470ae1cd4ebe45980cab14fb999d87c2a904e
ArthurMaverick <arthuracs18@gmail.com>
2021-11-02 10:59:53 -0300

    add - example - 1

--- cloudformationAndnode/index.ts
+++ cloudformationAndnode/index.ts
@@ -1,2 +1,22 @@
 import {CloudFormation} from 'aws-sdk'
 
+
+const values:  CloudFormation.CreateStackInput = {
+  Parameters: [
+    {ParameterKey: 'KeyName', ParameterValue: 'myKeyPair'}, 
+  ] , 
+StackName: 'RANDOM'
+}
+
+
+function CreateStack(template: CloudFormation.CreateStackInput) {
+  const cfn = new CloudFormation({apiVersion: '2010-09-09'})
+  cfn.createStack(template, callback => console.log(callback))
+}
+
+function template (params: CloudFormation.CreateStackInput) {
+  return params
+}
+
+
+CreateStack(template(values))
